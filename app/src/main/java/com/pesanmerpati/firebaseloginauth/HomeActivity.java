cc559a17bddadc1c55b210f94d83bb74ef7b91da
rohmts <rohmatsuseno@gmail.com>
2018-03-26 17:10:18 +0700

    Initial commit

---
+++ app/src/main/java/com/pesanmerpati/firebaseloginauth/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.pesanmerpati.firebaseloginauth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        findViewById(R.id.button_log_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/pesanmerpati/firebaseloginauth/MainActivity.java
@@ -0,0 +1,139 @@
+package com.pesanmerpati.firebaseloginauth;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditTextPhone, mEditTextCode;
+    Button mButtonGetCode, mButtonConfirmCode;
+
+    //firebase authentication
+    FirebaseAuth mAuth;
+
+    String codeVerfication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        mEditTextPhone = findViewById(R.id.editText_phone);
+        mEditTextCode = findViewById(R.id.editText_code);
+
+        mButtonGetCode = findViewById(R.id.button_get_code);
+        mButtonConfirmCode = findViewById(R.id.button_confirm_code);
+
+        mButtonGetCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendVerificationCode();
+            }
+        });
+
+        mButtonConfirmCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CodeSignInVerification();
+            }
+        });
+
+    }
+
+    private void CodeSignInVerification() {
+
+        String code = mEditTextCode.getText().toString();
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeVerfication, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = task.getResult().getUser();
+
+                            Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                            finish();
+                            // ...
+                        } else {
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(MainActivity.this, "verification code doesn't match", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode() {
+
+        String phoneNumber = mEditTextPhone.getText().toString();
+
+        if (phoneNumber.isEmpty()) {
+            mEditTextPhone.setError("Phone number is required");
+            mEditTextPhone.requestFocus();
+            return;
+        }
+
+        if (phoneNumber.length() < 10) {
+            mEditTextPhone.setError("Please enter a valid phone number");
+            mEditTextPhone.requestFocus();
+            return;
+        }
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            codeVerfication = s;
+        }
+    };
+}
